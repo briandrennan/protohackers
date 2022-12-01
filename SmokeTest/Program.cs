031a15a504914f5f234f936e37c763c8223606ff
Brian Drennan <brian.e.drennan@outlook.com>
2022-12-01 00:07:19 -0500

    tree add0b1ccffa1a0eb43c17781a385caad1b147fa9 author Brian Drennan <brian.e.drennan@outlook.com> 1669870882 -0500 committer Brian Drennan <brian.e.drennan@outlook.com> 1669870882 -0500
    Initial stab at Protohackers 0
    
    This is my first forray into socket programming based on the problem
    sets from the Protohackers website. This first change provides an
    implementation of RFC 862 (TCP echo) based on the problem description
    located here:
    
    - https://protohackers.com/problem/0
    
    The code is a bit messy. I haven't looked at socket APIs in years. I
    also had a hard time getting the Windows Telnet client to work well
    for testing the server, so I created a custom client. The client
    supports reading data from the standard input stream to send to the
    server, but will fall back to prompting the user for input if the
    standard input stream has not been redirected. This provides a very
    Unix-y experience for transmitting data to the server. The server
    socket listener is on port 7. There is not currently a means for
    overriding that. It's also not terribly robust or resilient on either
    side. That may come in the future if I decide to continue down this
    path.

---
+++ SmokeTest/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Buffers;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("0. Smoke Test");
+
+
+using var cts = new CancellationTokenSource();
+Console.CancelKeyPress += Console_CancelKeyPress;
+
+var serverEndpoint = new IPEndPoint(IPAddress.Any, 7);
+using var server = new Socket(serverEndpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+server.Bind(serverEndpoint);
+server.Listen();
+Console.WriteLine("Listening on endpoint {0}", serverEndpoint);
+
+try
+{
+    while (!cts.IsCancellationRequested)
+    {
+        var client = await server.AcceptAsync(cts.Token).ConfigureAwait(false);
+        Console.WriteLine("Connection received from client {0}", client.RemoteEndPoint);
+        await HandleEchoAsync(client, cts).ConfigureAwait(false);
+    }
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Shutting down.");
+}
+
+async Task HandleEchoAsync(Socket client, CancellationTokenSource cts)
+{
+    var buffer = ArrayPool<byte>.Shared.Rent(1024);
+    try
+    {
+        var memory = buffer.AsMemory();
+        var bytesTransfered = await client.ReceiveAsync(memory, cts.Token).ConfigureAwait(false);
+
+        var send = memory[..bytesTransfered];
+        var clientMessage = Encoding.ASCII.GetString(send.ToArray());
+        Console.WriteLine("- {0}: received {1}", client.RemoteEndPoint, clientMessage);
+        await client.SendAsync(send, cts.Token).ConfigureAwait(false);
+        Console.WriteLine("- {0}: sent {1}", client.RemoteEndPoint, clientMessage);
+        var address = client.RemoteEndPoint;
+        await client.DisconnectAsync(true, cts.Token).ConfigureAwait(false);
+        Console.WriteLine("- {0}: (disconnected)", address);
+    }
+    finally
+    {
+        if (!cts.IsCancellationRequested)
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
+    }
+}
+
+void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+{
+    e.Cancel = true;
+    cts.Cancel();
+}
---
+++ TcpClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("TCP Client!");
+
+if (args.Length != 2)
+{
+    Console.WriteLine("Usage: [address] [port]");
+    return -1;
+}
+
+var address = IPAddress.Parse(args[0]);
+var port = int.Parse(args[1]);
+
+var endpoint = new IPEndPoint(address, port);
+var clientPort = (ushort)Random.Shared.Next(IPEndPoint.MaxPort - 10000, IPEndPoint.MaxPort - 1);
+Console.WriteLine("Connecting to server {0} from port {1}", endpoint, clientPort);
+var client = new IPEndPoint(IPAddress.Loopback, clientPort);
+var server = new Socket(client.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+await server.ConnectAsync(endpoint).ConfigureAwait(false);
+
+string message;
+if (!Console.IsInputRedirected)
+{
+    Console.Write("Message: ");
+    message = Console.ReadLine()!;
+}
+else
+{
+    message = Console.In.ReadToEnd();
+    Console.WriteLine("Send: {0}", message);
+}
+
+var bytes = Encoding.ASCII.GetBytes(message!);
+
+await server.SendAsync(bytes).ConfigureAwait(false);
+var buffer = new ArraySegment<byte>(new byte[4096]);
+var response = server.ReceiveAsync(buffer).ConfigureAwait(false);
+message = Encoding.ASCII.GetString(buffer);
+Console.WriteLine("Response: {0}", message);
+
+return 0;
